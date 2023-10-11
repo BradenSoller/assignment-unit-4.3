651e9b2470b7fdeed94a932d48c70056fbdb2481
Braden Soller <braden.soller18@gmail.com>
2023-10-11 18:25:36 -0500

    finished first three problems

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,20 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+let item = 'kale';
 
+function addItem(item){
+basket.push(item);
+return true; 
+}
+ console.log(addItem(item));
 
-
+function listItems(basket) {
+    for (let i=0; i<basket.length; i++){
+        console.log(basket[i]);
+    }
+}
 
 
 
